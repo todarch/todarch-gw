87a976484e2bc569bb77b4db74ac06e3b779d53c
selimssevgi <selimssevgi@gmail.com>
2018-06-06 23:54:40 +0300

    set logout cookie

---
+++ src/main/java/com/todarch/gw/jwt/LogoutPostFilter.java
@@ -0,0 +1,38 @@
+package com.todarch.gw.jwt;
+
+import com.todarch.gw.common.GwPostFilter;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Component
+@AllArgsConstructor
+@Slf4j
+public class LogoutPostFilter extends GwPostFilter {
+
+  private final JwtUtil jwtUtil;
+
+  private static final String PARTIAL_LOGOUT_ENDPOINT = "logout";
+
+  @Override
+  public boolean shouldFilter() {
+    return isLogoutEndpoint();
+  }
+
+  private boolean isLogoutEndpoint() {
+    String requestURI = request().getRequestURI();
+    if (requestURI != null) {
+      log.info("Is logging endpoint: {}", requestURI);
+      return requestURI.toLowerCase().contains(PARTIAL_LOGOUT_ENDPOINT);
+    }
+    log.info("Was not logout endpoint");
+    return false;
+  }
+
+  @Override
+  public Object run() {
+    response().addCookie(jwtUtil.getLogoutCookie());
+    log.info("Set logout cookie");
+    return null;
+  }
+}
